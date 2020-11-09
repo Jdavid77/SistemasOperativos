0ff0ded4c99743e393096523fffeb25a988c41aa
Igoptt <rodrigo.severim.10@gmail.com>
2020-11-09 20:24:27 +0000

    configuracoes

--- monitor.c
+++ monitor.c
@@ -1,3 +1,16 @@
+#include "bibliotecas.h"
 main (){
     
+}
+
+int escreve(){  
+        int fd = open("monitor.log", O_WRONLY |O_CREAT, S_IRUSR | S_IWUSR);
+        if(fd == 0){ // Se o ficheiro nao foi aberto ou criado
+            printf("Erro ao abrir o ficheiro");
+                return -1; // Retorna a indicação de erro
+
+        }
+        else{
+            
+        }
 }
