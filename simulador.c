e2f0c1719151bc756969a975191419f6bfe14df6
Igoptt <rodrigo.severim.10@gmail.com>
2020-11-09 21:18:54 +0000

    funcao escreve_ficheiro do simulador

---
+++ simulador.c
@@ -0,0 +1,19 @@
+#include "bibliotecas.h"
+
+int escreve_ficheiro(char texto2[]){
+        FILE *fp;
+        fp = fopen("ficheiro_simulador.txt","a"); //Abre o ficheiro no modo de write para escrever no fim
+        if(fp == NULL){ //Caso o ficheiro nao exista ou nao seja aberto
+                printf("Erro ao abrir o ficheiro");
+                return -1; //Indicaçao do erro
+        }
+        else{
+                fprintf(fp,texto2); //Escreve o text no ficheiro
+                fclose(fp); //fecha o ficheiro
+                if(fclose(fp) == EOF){ //Quando ocorre um erro a fechar o ficheiro fclose retorna EOF
+                        printf("Erro ao fechar o ficheiro");
+                        return 1; //Indicacao do erro
+                }
+                return 0; //retorna 0 para indicar que funcionou corretamente
+        }
+}
