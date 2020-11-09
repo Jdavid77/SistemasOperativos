acdb63ab68c62a996d9fa5c7c35d1f970844f405
Igoptt <rodrigo.severim.10@gmail.com>
2020-11-09 21:20:03 +0000

    bugfix

--- monitor.c
+++ monitor.c
@@ -3,17 +3,6 @@ main (){
     
 }
 
-/*int escreve(){  
-        int fd = open("monitor.log", O_WRONLY |O_CREAT, S_IRUSR | S_IWUSR);
-        if(fd == 0){ // Se o ficheiro nao foi aberto ou criado
-            printf("Erro ao abrir o ficheiro");
-                return -1; // Retorna a indicação de erro
-
-        }
-        else{
-            
-        }
-}*/
 /* Escrever no ficheiro E no monitor */
 int escreve_monitor_ficheiro(char texto[]){
         FILE *fp;
