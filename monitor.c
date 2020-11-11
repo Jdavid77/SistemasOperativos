cdbe71e5d1c8dcd95bc733600ee99929196bda68
jdavid77 <jdavid72009@hotmail.com>
2020-11-11 22:30:12 +0000

    final

--- monitor.c
+++ monitor.c
@@ -16,15 +16,15 @@ int escreve_monitor_ficheiro(char texto[]){
 int le(){
         #define MAXSIZE 512
 
-        FILE *apontador;
-        apontador = fopen("ficheiro_teste.txt", "r");
-        char linha[MAXSIZE];
-        while(!feof(apontador)){
+        FILE *apontador; //cria apontador para abrir o ficheiro
+        apontador = fopen("ficheiro_teste.txt", "r"); // abre o ficheiro para leitura
+        char linha[MAXSIZE]; 
+        while(!feof(apontador)){ // enquanto não chegar ao fim do ficheiro
 
-                fgets(linha, MAXSIZE , apontador);
-                printf(linha);
+                fgets(linha, MAXSIZE , apontador); // lê apenas uma linha do ficheiro
+                printf(linha); // dá o print dessa mesma linha
                 }
-                fclose(apontador);
+                fclose(apontador); // fecha o ficheiro
         
         }
 int main ( int argc , char const * argv[] ) {
