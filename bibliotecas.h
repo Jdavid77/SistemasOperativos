a622882fbc4a876ee8741bf29e78a5bcc80a06aa
jlpmartins98 <jm8244936@gmail.com>
2020-11-15 13:03:00 +0000

    bilbioteac update

--- bibliotecas.h
+++ bibliotecas.h
@@ -1,6 +1,5 @@
 #include <stdio.h>          //Biblioteca geral de input e outputs da linguagem C
 #include <stdlib.h>         //Biblioteca padrão do C
-#include <sys/types.h>      //Contem definicoes de data types usados nos system calls. E necessario para os dois procimos imports
-#include <time.h>            //biblioteca para trabalhar com  o tempo
-#include <stdbool.h>        //Para trabalhar com boleanos
-#include <ctype.h>
+#include <sys/types.h>      //Contem definicoes de data types usados nos system calls
+#include <sys/stat.h>
+#include <fcntl.h>       
